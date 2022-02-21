0853baf8240e20ec141e5ade3b76289d3f764cbf
murat <muratabinpro@gmail.com>
2022-02-21 19:01:50 +0300

    New font and player resized

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const countDownDate = new Date("June 18, 2023 07:00:00 GMT+03:00");
+const electionDate = new Date("June 18, 2023 07:00:00 GMT+03:00");
 const mainElement = document.getElementById("main")
 
 const links = [
     'https://www.youtube.com/embed/zZMrkJQRq-4?autoplay=1',
     'https://www.youtube.com/embed/iqqdFglJ_zA?autoplay=1'
 ]
-const pickRandomLink = Math.floor(Math.random() * links.length);
 
-const count = setInterval(function () {
+const count = setInterval(() => {
     const now = new Date();
-    const distance = countDownDate - now;
+    const distance = electionDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / 86400000);
+    const hours = Math.floor((distance % 86400000) / 3600000);
+    const minutes = Math.floor((distance % 3600000) / 60000);
+    const seconds = Math.floor((distance % 60000) / 1000);
 
     mainElement.innerHTML = `
         <div>
-            <h1 id="count" class="fs-4rem fw-bolder main-text">
+            <h1 class="fs-5rem fw-bolder main-text">
                 ${days} Gün ${hours} Saat ${minutes} Dakika ${seconds} Saniye
            </h1>
-           <h1 class="pt-3">Sonra seçim var!</h1>
+           <h1 class="pt-sm-3 fs-1">Sonra seçim var!</h1>
         </div>
     `;
     if (distance <= 0) {
+        const link = Math.floor(Math.random() * links.length)
+
         mainElement.innerHTML = `
         <div>
             <div class="p-5">
@@ -34,7 +35,7 @@ const count = setInterval(function () {
             <div class="position-relative player">
                 <div class="ratio ratio-16x9">
                         <iframe class="border-0" type="text/html"
-                            src="${links[pickRandomLink]}">
+                            src="${links[link]}">
                         </iframe>
                 </div>
             </div>
@@ -42,5 +43,6 @@ const count = setInterval(function () {
         clearInterval(count);
     }
 }, 1000);
-console.error('%cBir hata ile karşılaşırsan bana bildirir misin? :=)', 'font-size: 1.25rem; color: white')
-console.error('%chttps://github.com/MuratAAydin', 'font-size: 1.25rem; color: white')
+console.group('%cBir hata ile karşılaşırsan bana bildirir misin? :=)', 'font-size: 1.25rem; color: white')
+console.error('%chttps://github.com/MuratAAydin', 'font-size: 1.25rem;')
+console.groupEnd()
